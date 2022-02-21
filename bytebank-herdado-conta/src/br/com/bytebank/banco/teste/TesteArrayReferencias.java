ba63bdb6b78cd4c0dfd61c4e3b312447c7b19872
Matheus Matos <matheusmatos.221@hotmail.com>
2022-02-21 11:48:22 -0300

    ADD coments + RENAME refs + USE type cast

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -14,15 +14,29 @@ import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 	public static void main(String[] args) {
+		// cria novo array
 		Conta[] contas = new Conta[5];
 		
-		ContaCorrente cc1 = new ContaCorrente(123, 12);
-		ContaCorrente cc2 = new ContaCorrente(123, 24);
+		// cria novas instancias de conta corrente e poupanca
+		ContaCorrente cc = new ContaCorrente(123, 12);
+		ContaPoupanca cp = new ContaPoupanca(123, 24);
 		
-		contas[0] = cc1;
-		contas[1] = cc2;
+		// insere referencias no array
+		contas[0] = cc;
+		contas[1] = cp;
 		
+		// nao compila
+		// ContaCorrente ref = contas[0];
+		
+		// compila porque faz uso do type cast
+		ContaCorrente ref = (ContaCorrente) contas[0];
+
+		// ClassCastException ContaPoupanca in ContaCorrente type
+		//ContaCorrente ref1 = (ContaCorrente) contas[1];
+		
+		// console.log
 		System.out.println(contas[0].getNumero());
+		System.out.println(ref.getNumero());
 		
 	}
 }
