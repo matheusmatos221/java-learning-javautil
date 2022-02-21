529639047e4e6057f0354cf91db01a55f4514a49
Matheus Matos <matheusmatos.221@hotmail.com>
2022-02-21 10:24:36 -0300

    Refatora class teste apra TesteObject e cria um array idades com loop para criacao de valores e consolelog para exibir os valores

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteObject.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteObject.java
@@ -3,7 +3,7 @@ package br.com.bytebank.banco.teste;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 
-public class Teste {
+public class TesteObject {
 	public static void main(String[] args) {
 		ContaCorrente cc = new ContaCorrente(123, 1);
 		ContaPoupanca cp = new ContaPoupanca(123, 2);
