453a1e44835c30816453ddab38e0c2159832ffcd
Matheus Matos <matheusmatos.221@hotmail.com>
2022-02-21 11:42:29 -0300

    REFACTOR imports and javadocs and RENAME array ContasCorrentes to contas

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,18 +1,28 @@
+/**
+ * Classe de teste para arrays e referencias
+ * @author Matheus
+ *
+ */
+
 package br.com.bytebank.banco.teste;
 
+// Imports
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
+
 
 public class TesteArrayReferencias {
 	public static void main(String[] args) {
-		ContaCorrente[] contasCorrentes = new ContaCorrente[5];
+		Conta[] contas = new Conta[5];
 		
 		ContaCorrente cc1 = new ContaCorrente(123, 12);
 		ContaCorrente cc2 = new ContaCorrente(123, 24);
 		
-		contasCorrentes[0] = cc1;
-		contasCorrentes[1] = cc2;
+		contas[0] = cc1;
+		contas[1] = cc2;
 		
-		System.out.println(contasCorrentes[0].getNumero());
+		System.out.println(contas[0].getNumero());
 		
 	}
 }
